b0f32be1221e28bc2478c41839dae348678b2c43
Spencer <spencerjpotts@gmail.com>
2019-03-23 01:55:04 +1100

    Added comments. And uncommented this.addNewItem(); function.

--- javascripts/InfiniteScroller/InfiniteScroller.v0.0.1.js
+++ javascripts/InfiniteScroller/InfiniteScroller.v0.0.1.js
@@ -7,10 +7,14 @@ Version: v0.0.1.js.
 
 Example Usage: 
 
-    window.onload = new InfiniteScroller({
-       debug: false,
-       selector: '#infinite'
-    }, true);
+    window.onload = function(event) {
+        let options = {
+        debug: false,
+        selector: "#infinite"
+        };
+
+        let infiniteScroller = new InfiniteScroller(options);
+    };
 
 */
 
@@ -47,8 +51,8 @@ var InfiniteScroller = function(options) {
   // Save the existing items.
   /*
   * get each '<li>' element from the list specified in the current context (self/this). 
-  * with each list item, push it to the items list. 
-  * using Array prototype so that each list item can be later used or copied.
+  * with each list item, push it to the items list attribute. 
+  * Using Array prototype so that each list item can be later used or copied.
   */
   Array.prototype.forEach.call(this.list.getElementsByTagName("li"), (item) => {
     this.items.push(item);
@@ -59,7 +63,7 @@ var InfiniteScroller = function(options) {
 
 
   /*
-  *
+  * 
   *
   * 
   */
@@ -67,19 +71,33 @@ var InfiniteScroller = function(options) {
     if (this.options.loadEnabled == true && this.isLoading == false) {
       this.isLoading = true;
       this.log("Loading content...");
+
+      // create a new text node with the options loading text supplied by options variable.
       var content = document.createTextNode(this.options.content);
+      
+      // create the a new list element 'li'.
       var template = document.createElement("li");
+      // add the css style loading class 'lading.' to the created template element.
       template.classList.add("loading");
+      // append the newly created text node to the 'li' template variable.
       template.appendChild(content);
 
       var loading = this.list.appendChild(template);
+
+      // get the list context and set its scoller to the bottom. 
+      // this will help the loading content appear more smoothly.
       this.view.scrollTop = this.view.scrollHeight;
-      setTimeout(()=> {
+
+      // set a timeout for the loading template to be removed.
+      // when the 2 second wait has ended set the 'isLoading' variable to false.
+      // and then remove the added 'list' loading template.
+      // then add the original set of list items into the 'list' using 'this.addNewItem()' function.      
+      setTimeout( () => {
         this.isLoading = false;
         loading.remove();
         this.addNewItem();
       }, 2000);
-      // loading.remove();
+      
     }
   };
 
@@ -116,7 +134,7 @@ var InfiniteScroller = function(options) {
     if (this.view.scrollTop + this.view.clientHeight >= this.view.scrollHeight) {
       this.log(this)
       this.loading();
-      // this.addNewItem();
+      this.addNewItem();
     }
   });
 
